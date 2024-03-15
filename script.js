b101134c8660e4fce0cb2197607e5e1b07e6abb5
Shaikh Mohammed Talib <shaikhtalib2705@gmail.com>
2024-03-15 23:24:29 +0530

    added text to speech functionality and improve some stylings

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // jQuery
 
 // console.log($)
+/*
 $(document).ready(function () {
     $('#btn').click(function (e) {
         e.preventDefault()
@@ -17,28 +18,55 @@ $(document).ready(function () {
         $('#cont').html(iHtml)
     })
 })
+*/
+
 
 
 // JavaScript 
-/*
-let btn = document.getElementById('btn')
-btn.addEventListener('click', (e) => {
+// 5 3s are 15
+let getBtn = document.getElementById('getBtn')
+let emp = []
+getBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    let inp = document.getElementById('inp')
-    let cont = document.getElementById('cont')
+    let inp = document.getElementById('inp');
+    let cont = document.getElementById('cont');
+    let h1 = document.querySelector('h1');
+    h1.innerHTML = `Table of ${inp.value}`;
     let iHtml = '';
     if (inp.value <= 0) {
         iHtml = `<div class="table text-center"><h2>Invalid Input</h2></div>`
     }
     else {
-        for (let i = 1; i <= 20; i++) {
+        for (let i = 1; i <= 12; i++) {
             // console.log(i + 1)
             iHtml += `
                     <div class="table text-center">${inp.value} x ${i} = ${inp.value * i}</div>
                 `
             // console.log(`${inp.value} x ${i} = ${inp.value * i}`)
+            emp.push(` ${inp.value} ${i}s are ${inp.value * i}. `);
         }
     }
     cont.innerHTML = iHtml;
+    inp.value = '';
+    // console.log(emp);
+
+    let playBtn = document.getElementById('playBtn');
+    playBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        // console.log(emp)
+        let speech = new SpeechSynthesisUtterance(emp)
+        window.speechSynthesis.speak(speech)
+    })
+
 })
-*/
+
+
+// let playBtn = document.getElementById('playBtn');
+// playBtn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     console.log(456)
+//     if (emp == []) {
+//         alert('Please fill')
+//     }
+// })
+
