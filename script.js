b10d0e2f050aaa61950bf8a43734f67c22a9f752
Shaikh Mohammed Talib <shaikhtalib2705@gmail.com>
2024-03-15 23:48:34 +0530

    fix some bugs

--- script.js
+++ script.js
@@ -24,15 +24,20 @@ $(document).ready(function () {
 
 // JavaScript 
 // 5 3s are 15
-let getBtn = document.getElementById('getBtn')
-let emp = []
+let getBtn = document.getElementById('getBtn');
+let emp = [];
+let playBtn = document.getElementById('playBtn');
+// console.log(playBtn)
+playBtn.style.display = 'none';
+
 getBtn.addEventListener('click', (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    let playBtn = document.getElementById('playBtn');
     let inp = document.getElementById('inp');
     let cont = document.getElementById('cont');
     let h1 = document.querySelector('h1');
-    h1.innerHTML = `Table of ${inp.value}`;
     let iHtml = '';
+    inp.value <= 0 ? h1.innerHTML = 'Multiplication Table' : h1.innerHTML = `Table of ${inp.value}`;
     if (inp.value <= 0) {
         iHtml = `<div class="table text-center"><h2>Invalid Input</h2></div>`
     }
@@ -40,21 +45,21 @@ getBtn.addEventListener('click', (e) => {
         for (let i = 1; i <= 12; i++) {
             // console.log(i + 1)
             iHtml += `
-                    <div class="table text-center">${inp.value} x ${i} = ${inp.value * i}</div>
-                `
+                <div class="table text-center">${inp.value} x ${i} = ${inp.value * i}</div>
+            `
             // console.log(`${inp.value} x ${i} = ${inp.value * i}`)
             emp.push(` ${inp.value} ${i}s are ${inp.value * i}. `);
         }
+        playBtn.style.display = 'block';
     }
     cont.innerHTML = iHtml;
     inp.value = '';
     // console.log(emp);
-
-    let playBtn = document.getElementById('playBtn');
+    
     playBtn.addEventListener('click', (e) => {
         e.preventDefault();
-        // console.log(emp)
         let speech = new SpeechSynthesisUtterance(emp)
+        // console.log(emp)
         window.speechSynthesis.speak(speech)
     })
 
